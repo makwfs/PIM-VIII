0bce687caf0d878ea7ad98e64f975b1555eeaf34
Mak Wercoutter <61856612+makwfs@users.noreply.github.com>
2020-11-09 23:34:04 -0300

    Ajuste do projeto para o modelo de três camadas

---
+++ PIM VIII V1/Modelo/Controle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Controle
+    {
+        public String Cadastrar (String nome, String cpf, int endereco);
+
+    }
+}
