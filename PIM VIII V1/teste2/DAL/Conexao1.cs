8e7b934353ff59bf6c52d1158ed4147ff79a8c26
Mak Wercoutter <61856612+makwfs@users.noreply.github.com>
2020-11-10 00:08:48 -0300

    Conexão com BD

--- PIM VIII V1/teste2/DAL/Conexao1.cs
+++ PIM VIII V1/teste2/DAL/Conexao1.cs
@@ -7,5 +7,32 @@ using System.Threading.Tasks;
 
 namespace Apresentacao.DAL
 {
+    public class Conexao
+    {
+        SqlConnection con = new SqlConnection(); //
+
+        public Conexao() // construtor
+        {
+            con.ConnectionString = @"Data Source=DESKTOP-9IFV0OT;Initial Catalog=BD_PIM;Integrated Security=True";
+        }
+
+        public SqlConnection Conectar()
+        {
+            if (con.State == System.Data.ConnectionState.Closed) // Se a conexao estiver fechada ira abrir a conexao
+            {
+                con.Open();
+            }
+            return con;
+        }
+
+
+        public void Desconectar()
+        {
+            if (con.State == System.Data.ConnectionState.Open) // Se a conexao estiver aberta entao ira fechar
+            {
+                con.Close();
+            }
+        }
+    }
    
 }
