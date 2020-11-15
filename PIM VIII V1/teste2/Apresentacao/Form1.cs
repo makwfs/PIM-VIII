62e87ada5d664e04e9b97734c717f5a7b5196876
Mak Wercoutter <61856612+makwfs@users.noreply.github.com>
2020-11-15 16:54:44 -0300

    botão-cadastrar-teste
    Feita primeira parte do btn_cadastro

--- PIM VIII V1/teste2/Apresentacao/Form1.cs
+++ PIM VIII V1/teste2/Apresentacao/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Teste2.Modelo;
+using System.Data.SqlClient;
 
 namespace Teste2
 {
@@ -28,19 +29,51 @@ namespace Teste2
             Application.Exit();
         }
 
-        private void btnCadastrar_Click(object sender, EventArgs e)
+        protected void btnCadastrar_Click(object sender, EventArgs e)
         {
-            controle controle = new controle();
-            String mensagem = controle.Cadastrar(txbNome.Text, txbCpf.Text);
-            if (controle.tem)
-            {
-                MessageBox.Show(mensagem, "Cadastro Realizado!", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-            else
-            {
-                MessageBox.Show(controle.mensagem);
-            }
+            string mensagem = "";
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = @"Data Source=DESKTOP-9IFV0OT;Initial Catalog=BD_PIMVIII;Integrated Security=True"; // endereço banco de dados
+            //definição do comando sql
+            string sql = "insert into pessoa (nome,cpf)  VALUES (@nome, @cpf)";
             
+    try
+            {
+                //Cria uma objeto do tipo comando passando como
+                
+                SqlCommand comando = new SqlCommand(sql, conn);
+                //Adicionando o valor das textBox nos parametros do comando
+                comando.Parameters.Add(new SqlParameter("@nome", this.txbNome.Text));
+                comando.Parameters.Add(new SqlParameter("@cpf", this.txbCpf.Text));
+               
+                //abre a conexao
+                conn.Open();
+                //executa o comando com os parametros que foram adicionados acima
+                comando.ExecuteNonQuery();
+                //fecha a conexao
+                conn.Close();
+                //Minha função para limpar os textBox
+                LimpaCamos();
+                //Abaixo temos a ultlização de javascript para apresentar ao usuário um alert
+                // referente ao msgbox
+                //RegisterClientScriptBlock("cadastrado", "<script> alert(Operação concluida!)</ script > ");
+                MessageBox.Show (mensagem,"Cadastrado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch
+            {
+                MessageBox.Show(mensagem, "Nao funfa!", MessageBoxButtons.OK, MessageBoxIcon.Information); ;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        private void LimpaCamos()
+        {
+            this.txbNome.Text = "";
+            this.txbCpf.Text = "";
+           
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
